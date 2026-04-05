f4881080c29c220f39ce631a0876d1da72271b9c
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-04-05 15:08:06 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 #![allow(unused_imports)]
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-fn main() {}
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
+
+    println!("Listening on: {}", listener.local_addr()?);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Got connection from: {}", stream.peer_addr()?);
+            }
+            Err(e) => {
+                eprint!("Failed to accept connection: {e}");
+            }
+        }
+    }
+    Ok(())
+}
