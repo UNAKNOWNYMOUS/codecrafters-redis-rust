84f05216d50d65d13b0fae673268d535012ffb22
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-04-06 20:15:34 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
 #![allow(unused_imports)]
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
 
-    println!("Listening on: {}", listener.local_addr()?);
+    loop {
+        let (socket, addr) = listener.accept().await.unwrap();
+        println!("Client: {} connected!", addr);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("Got connection from: {}", stream.peer_addr()?);
-                handle_client(stream)?;
-            }
-            Err(e) => {
-                eprint!("Failed to accept connection: {e}");
-            }
-        }
+        tokio::spawn(async move {
+            handle_client(socket).await;
+        });
     }
-    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buf = [0u8; 4096];
+async fn handle_client(mut socket: TcpStream) {
+    let mut buf = [0u8; 1024];
+
     loop {
-        let bytes_read = stream.read(&mut buf)?;
+        let bytes_read = match socket.read(&mut buf).await {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(_) => break,
+        };
 
-        if bytes_read == 0 {
-            println!("Client disconnected!");
-            return Ok(());
-        }
-
-        stream.write_all(b"+PONG\r\n")?;
+        socket.write_all(b"+PONG\r\n").await.unwrap();
     }
 }
